1418fe55b61233526ef44e0523e19f145991025d
LyashenkoAndron <lyashenko.andron@mail.ru>
2022-11-25 20:29:23 +0300

    add stack0.c file

--- test_git/stack0.c
+++ test_git/stack0.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+	volatile int modified;
+	char buffer[64]; 
+
+	modified = 0;
+	fgets(buffer, sizeof(buffer), stdin);
+	if (modified != 0) {
+		printf("you have changed the 'modified' variable\n");
+	}
+	else {
+		printf("Try again?\n");
+	}
+
+}
